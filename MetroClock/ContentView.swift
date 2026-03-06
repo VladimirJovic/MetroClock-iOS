b6a2e1ec72e156fa0b35f4eda7cde3789f3d97fb
VLADA <vladajovic811@gmail.com>
2026-03-06 22:18:52 +0100

    Initial Commit

---
+++ MetroClock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetroClock
+//
+//  Created by Vlada Jovic on 6. 3. 2026..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetroClock/MetroClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetroClockApp.swift
+//  MetroClock
+//
+//  Created by Vlada Jovic on 6. 3. 2026..
+//
+
+import SwiftUI
+
+@main
+struct MetroClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
